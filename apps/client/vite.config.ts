238f64bdeb5e3d67d67a2f527f4a34f54387d70a
minseobshim <mshim@blast-team.com>
2025-03-26 20:38:58 +0900

    [Hotfix]Fix config issue

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import * as path from 'path';
+// @ts-ignore
 import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
