3a40c1645c76c7cf28b82f71de221e9dca54712b
minseobshim <mshim@blast-team.com>
2025-04-18 23:25:58 +0900

    [Feat]Setup basic navigations(TODO: fix "to" attribute)

--- apps/client/src/shared/components/MainHeader.tsx
+++ apps/client/src/shared/components/MainHeader.tsx
@@ -3,10 +3,16 @@ import MainHeaderLink from '@/shared/components/MainHeaderLink.tsx';
 const MainHeader = () => {
   return (
     <header className={'bg-black text-white py-6 px-4'}>
-      <ul className={'flex gap-4'}>
-        <MainHeaderLink to={'/'} content={'Home'} />
-        <MainHeaderLink to={'/users'} content={'Users'} />
-      </ul>
+      <nav>
+        <ul className={'flex gap-4'}>
+          <MainHeaderLink to={'/'} content={'HOME'} />
+          <MainHeaderLink to={'/users'} content={'ALL USERS'} />
+          {/*  TODO: fix "to" attributes */}
+          <MainHeaderLink to={'/u1/places'} content={'MY PLACES'} />
+          <MainHeaderLink to={'/places/new'} content={'ADD PLACES'} />
+          <MainHeaderLink to={'/auth'} content={'AUTHENTICATE'} />
+        </ul>
+      </nav>
     </header>
   );
 };
