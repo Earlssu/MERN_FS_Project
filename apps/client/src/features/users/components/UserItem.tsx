2787f34ed13344a903a384f07cc070f5e01575a3
minseobshim <mshim@blast-team.com>
2025-04-25 12:04:34 +0900

    [Fix]Fix html tags

--- apps/client/src/features/users/components/UserItem.tsx
+++ apps/client/src/features/users/components/UserItem.tsx
@@ -8,32 +8,40 @@ interface UserItemProps {
   lastUpdated: string;
 }
 
+import { useNavigate } from 'react-router-dom';
+
 const UserItem: React.FC<{ user: UserItemProps }> = ({ user }) => {
+  const navigate = useNavigate();
+
   return (
-    <a
-      className={'flex gap-4 border-b-1 border-gray-300 p-4 hover:bg-amber-50'}
-      href={`/user/${user.id}`}
+    <div
+      className="flex gap-4 border-b border-gray-300 p-4 hover:bg-amber-50 cursor-pointer"
+      onClick={() => navigate(`/user/${user.id}`)}
     >
       <div>
         <img
           src={user.imageUrl}
           alt={`${user.name}'s profile`}
-          className={'w-32 h-32 rounded-lg object-cover'}
+          className="w-32 h-32 rounded-lg object-cover"
         />
       </div>
-      <div className={'flex flex-col gap-2'}>
-        <h3 className={'text-xl text-slate-800 h-full'}>{user.name}</h3>
+      <div className="flex flex-col gap-2">
+        <h3 className="text-xl text-slate-800 h-full">{user.name}</h3>
         <div>방탈출 횟수: {user.placeCount}회</div>
         <div>
-          인생테마:
-          <a href={user.lifeThemeUrl} className={'text-indigo-500'} target="_blank">
-            {' '}
+          인생테마:{' '}
+          <a
+            href={user.lifeThemeUrl}
+            className="text-indigo-500"
+            target="_blank"
+            onClick={(e) => e.stopPropagation()}
+          >
             {user.lifeTheme}
           </a>
         </div>
         <div>마지막 방탈출: {user.lastUpdated}</div>
       </div>
-    </a>
+    </div>
   );
 };
 
