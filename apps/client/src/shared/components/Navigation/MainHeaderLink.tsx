533b8f3ad635d227bfbb74c7e26370e6d90c2244
minseobshim <mshim@blast-team.com>
2025-05-13 18:40:19 +0900

    [Feat]Integrate twMerge and clsx

--- apps/client/src/shared/components/Navigation/MainHeaderLink.tsx
+++ apps/client/src/shared/components/Navigation/MainHeaderLink.tsx
@@ -1,14 +1,19 @@
+import React from 'react';
+import { cx } from '@/shared/utils/cx.ts';
+
 interface MainHeaderLinkProps {
   to: string;
   content: string;
+  className?: string;
 }
 
-const MainHeaderLink: React.FC<MainHeaderLinkProps> = ({ to, content }) => {
+const MainHeaderLink: React.FC<MainHeaderLinkProps> = ({ to, content, className }) => {
   return (
     <li
-      className={
-        'font-bold text-lg hover:text-amber-400 border-b-2 px-2 pb-6 sm:p-0 border-gray-200 sm:border-none'
-      }
+      className={cx(
+        'font-bold text-lg hover:text-amber-400 border-b-2 px-2 pb-6 sm:p-0 border-gray-200 sm:border-none',
+        className,
+      )}
     >
       <a href={to}>{content ? content : 'Link Empty'}</a>
     </li>
