ef92826fc49bdf3a7f9a01a21a7c189cf7113464
minseobshim <mshim@blast-team.com>
2025-05-14 17:20:41 +0900

    [Feat]More Auth Context setup for edit/delete button @ ThemeItem.tsx

--- apps/client/src/features/themes/components/ThemeItem.tsx
+++ apps/client/src/features/themes/components/ThemeItem.tsx
@@ -1,16 +1,18 @@
 import { ThemeType } from '@/features/themes/types/themes.ts';
 import Button from '@/shared/components/FormElements/Button.tsx';
-import React, { lazy, Suspense, useState } from 'react';
+import React, { lazy, Suspense, useContext, useState } from 'react';
 import Modal from '@/shared/components/UIElements/Modal.tsx';
 import { getRateColor } from '@/features/themes/hooks/useThemeUtils.ts';
 import LoadingSpinner from '@/shared/components/UIElements/LoadingSpinner.tsx';
 import Card from '@/shared/components/UIElements/Card.tsx';
+import { AuthContext } from '@/shared/context/authContext.ts';
 
 interface ThemeItemProps extends ThemeType {}
 
 const Map = lazy(() => import('@/shared/components/UIElements/Map.tsx'));
 
 const ThemeItem: React.FC<{ theme: ThemeItemProps }> = ({ theme }) => {
+  const auth = useContext(AuthContext);
   const [showMap, setShowMap] = useState(false);
   const [showConfirmModal, setShowConfirmModal] = useState(false);
 
@@ -91,14 +93,16 @@ const ThemeItem: React.FC<{ theme: ThemeItemProps }> = ({ theme }) => {
               예약하러 가기
             </Button>
           </div>
-          <div className={'flex gap-4'}>
-            <Button style={'edit'} size={'sm'} to={`/themes/${theme.id}`}>
-              수정
-            </Button>
-            <Button style={'danger'} size={'sm'} onClick={showDeleteWarningHandler}>
-              삭제
-            </Button>
-          </div>
+          {auth.isLoggedIn && (
+            <div className={'flex gap-4'}>
+              <Button style={'edit'} size={'sm'} to={`/themes/${theme.id}`}>
+                수정
+              </Button>
+              <Button style={'danger'} size={'sm'} onClick={showDeleteWarningHandler}>
+                삭제
+              </Button>
+            </div>
+          )}
         </div>
       </Card>
     </React.Fragment>
