cd3049079d02f688e40eae7363ce7b3ddce98e87
minseobshim <mshim@blast-team.com>
2025-05-18 21:04:41 +0900

    [Fix]Fix tailwind css issue and install autoprefixer

---
+++ apps/client/src/shared/styles/colors.ts
@@ -0,0 +1,6 @@
+export const colors = {
+  primary: '#3F72AF',
+  secondary: '#112D4E',
+  tertiary: '#DBE2EF',
+  base: '#F9F7F7',
+};
--- apps/client/tailwind.config.ts
+++ apps/client/tailwind.config.ts
@@ -1,5 +1,14 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      colors: {
+        primary: '#3F72AF',
+        secondary: '#112D4E',
+        tertiary: '#DBE2EF',
+        base: '#F9F7F7',
+      },
+    },
+  },
   plugins: [],
 };
