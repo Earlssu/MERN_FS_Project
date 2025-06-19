b1f3d1fdb44955eb372a9ccd0d6fce5ec8832d26
minseobshim <mshim@blast-team.com>
2025-06-19 21:37:45 +0900

    [Misc]Staging missing commit

---
+++ packages/shared/src/types/users.ts
@@ -0,0 +1,11 @@
+export type UserType = {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+  imageUrl?: string;
+  placeCount?: number;
+  lifeTheme?: string;
+  lifeThemeUrl?: string;
+  lastUpdated?: string;
+}; 
